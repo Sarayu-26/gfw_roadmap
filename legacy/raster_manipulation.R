e67a539991a2c1f4cdd3ebb0898d6c879d702750
IsaakBM <ibrito@conservation.org>
2025-09-23 13:19:34 -0700

    Comment out rasterization and plotting code
    Rasterization and plotting steps have been commented out, and the data source file has been updated to 'agg_cell_fishing_full.txt'. This change may be for debugging, refactoring, or to temporarily disable visualization and raster creation.

--- legacy/raster_manipulation.R
+++ legacy/raster_manipulation.R
@@ -2,14 +2,14 @@ library(terra)
 library(dplyr)
 library(maps)
 
-data <- read.csv("outputs/agg_cell_dredge_fishing_full.txt", header = TRUE, sep = "\t")
-pts <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")
-r <- rast(pts, resolution = 0.01)
-r_fish <- rasterize(pts, r, field = "fishing_hours_sum", fun = "sum")
-
-plot(r_fish)
-map("world", add = TRUE) 
-points(pts, col = "red", pch = 20)
+data <- read.csv("outputs/agg_cell_fishing_full.txt", header = TRUE, sep = "\t")
+# pts <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")
+# r <- rast(pts, resolution = 0.01)
+# r_fish <- rasterize(pts, r, field = "fishing_hours_sum", fun = "sum")
+# 
+# plot(r_fish)
+# map("world", add = TRUE) 
+# points(pts, col = "red", pch = 20)
 
 data <- data %>% 
   dplyr::select(lon, lat, fishing_hours_sum)
