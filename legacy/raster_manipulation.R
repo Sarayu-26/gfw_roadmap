ef290f8076ebcd8dd8d8b362ef062559a9f5c24b
IsaakBM <ibrito@conservation.org>
2025-09-23 11:35:08 -0700

    Add legacy raster manipulation script
    Introduces raster_manipulation.R for reading, rasterizing, and plotting fishing data using terra, dplyr, and maps. This script visualizes fishing hours and geographic points from a tab-separated data file.

---
+++ legacy/raster_manipulation.R
@@ -0,0 +1,17 @@
+library(terra)
+library(dplyr)
+library(maps)
+
+data <- read.csv("outputs/agg_cell_dredge_fishing_full.txt", header = TRUE, sep = "\t")
+pts <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")
+r <- rast(pts, resolution = 0.01)
+r_fish <- rasterize(pts, r, field = "fishing_hours_sum", fun = "sum")
+
+plot(r_fish)
+map("world", add = TRUE) 
+points(pts, col = "red", pch = 20)
+
+data <- data %>% 
+  dplyr::select(lon, lat, fishing_hours_sum)
+r <- rast(data, type = "xyz", crs = "EPSG:4326")
+plot(r)
