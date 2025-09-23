cce2dd11a58a1fa07d51f023d37511937189ceca
IsaakBM <ibrito@conservation.org>
2025-09-23 13:25:17 -0700

    Update input data source in raster_manipulation.R
    Changed the CSV file loaded to 'agg_cell_drifting_longlines_full.txt' instead of 'agg_cell_fishing_full.txt' to use a different dataset for raster manipulation.

--- legacy/raster_manipulation.R
+++ legacy/raster_manipulation.R
@@ -2,7 +2,7 @@ library(terra)
 library(dplyr)
 library(maps)
 
-data <- read.csv("outputs/agg_cell_fishing_full.txt", header = TRUE, sep = "\t")
+data <- read.csv("outputs/agg_cell_drifting_longlines_full.txt", header = TRUE, sep = "\t")
 # pts <- vect(data, geom = c("lon", "lat"), crs = "EPSG:4326")
 # r <- rast(pts, resolution = 0.01)
 # r_fish <- rasterize(pts, r, field = "fishing_hours_sum", fun = "sum")
