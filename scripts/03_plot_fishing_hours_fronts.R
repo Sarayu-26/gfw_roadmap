33835aa53ab001707ad7753ac26843e4555e80de
IsaakBM <ibrito@conservation.org>
2025-12-15 13:06:57 -0800

    Add script to plot fishing hours with fronts overlay
    Introduces 03_plot_fishing_hours_fronts.R to visualize fishing hours data with log scaling, overlaying ocean fronts and land polygons. Includes masking and custom color scaling for improved interpretability.

---
+++ scripts/03_plot_fishing_hours_fronts.R
@@ -0,0 +1,81 @@
+library(terra)
+rs <- readRDS("outputs/combined_masked_sum.rds")
+# plot(log10(rs)+1)
+land <- get_world_latlon()
+df <- as.data.frame(rs, xy = TRUE, na.rm = FALSE)
+colnames(df) <- c("x", "y", "val")
+
+ggplot() +
+  geom_raster(
+    data = df,
+    aes(x = x, y = y, fill = val),
+    na.rm = TRUE
+  ) +
+  scale_fill_viridis_c(
+    trans = scales::log10_trans(),
+    na.value = NA
+  ) +
+  geom_sf(
+    data = front_poly,
+    fill = NA,
+    color = "red",
+    linewidth = 0.2
+  ) +
+  geom_sf(
+    data = land,
+    fill = "grey20",
+    color = "grey30",
+    linewidth = 0.2
+  ) +
+  coord_sf(expand = FALSE)
+
+
+#########################################################################################################################
+
+df <- as.data.frame(rs, xy = TRUE, na.rm = FALSE)
+colnames(df) <- c("x", "y", "val")
+df_sf <- st_as_sf(
+  df,
+  coords = c("x", "y"),
+  crs = 4326,
+  remove = FALSE
+)
+
+sf::sf_use_s2(FALSE)
+inside <- st_intersects(df_sf, front_poly, sparse = FALSE)[, 1]
+df_masked <- df_sf[inside, ]
+
+log10p1 <- scales::trans_new(
+  name = "log10p1",
+  transform = function(x) log10(x + 1),
+  inverse   = function(x) 10^x - 1
+)
+brks <- c(0, 1, 10, 100, 1e3, 1e4, 1e5, 1e6)
+
+
+ggplot() +
+  geom_tile(
+    data = df_masked,
+    aes(x = x, y = y, fill = val),
+    na.rm = TRUE
+  ) +
+  scale_fill_viridis_c(
+    name     = "log10 + 1 fishing hours",
+    trans    = log10p1,
+    breaks   = brks,
+    labels   = scales::label_number(scale_cut = scales::cut_si("")),
+    na.value = NA
+  ) +
+  geom_sf(
+    data = front_poly,
+    fill = NA,
+    color = "red",
+    linewidth = 0.3
+  ) +
+  geom_sf(
+    data = land,
+    fill = "grey20",
+    color = "grey30",
+    linewidth = 0.2
+  ) +
+  coord_sf(expand = FALSE)
